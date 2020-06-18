b1a614bd4681959a1b16d613a3e6126e05049eca
puttapraveen <45269804+puttapraveen@users.noreply.github.com>
2020-06-18 17:22:35 +0530

    Add files via upload

---
+++ js/j.js
@@ -0,0 +1,6 @@
+function change(){
+    document.getElementById("hea").innerHTML="DO CHECK MY PROFILE BELOW!"
+}
+function rechange(){
+    document.getElementById("hea").innerHTML="HELLO!  I AM GLAD YOU VISITED"
+}
